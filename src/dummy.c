51330adccfc850dd774a9c20f097e76c26ab3763
Pawel Karczewski <pawel.karczewski@intel.com>
2021-11-09 16:14:09 +0100

    [deleteme] Add dummy code

---
+++ src/dummy.c
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2021, Intel Corporation
+
+// wow wow much code
+
+int pmemstream_XXX();
---
+++ src/include/dummy.h
@@ -0,0 +1,2 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2021, Intel Corporation
