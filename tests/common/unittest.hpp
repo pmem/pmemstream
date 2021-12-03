4821bd899054283f5edb60ffedcca7b3b29b4042
Igor Chorążewicz <igor.chorazewicz@intel.com>
2021-12-03 15:48:29 +0100

    Add append test

--- tests/common/unittest.hpp
+++ tests/common/unittest.hpp
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <type_traits>
 
 static inline void UT_EXCEPTION(std::exception &e)
@@ -79,5 +80,27 @@ struct return_check {
 
 	bool status = true;
 };
+
+auto make_pmemstream(const std::string &file, size_t block_size, size_t size)
+{
+	struct pmem2_map *map = map_open(file.c_str(), size);
+	if (map == NULL) {
+		throw std::runtime_error(pmem2_errormsg());
+	}
+
+	auto map_delete = [](struct pmem2_map *map) { pmem2_map_delete(&map); };
+	auto map_uptr = std::unique_ptr<struct pmem2_map, decltype(map_delete)>(map, map_delete);
+
+	struct pmemstream *stream;
+	int ret = pmemstream_from_map(&stream, block_size, map);
+	if (ret == -1) {
+		throw std::runtime_error("pmemstream_from_map failed");
+	}
+
+	auto stream_delete = [map_uptr = std::move(map_uptr)](struct pmemstream *stream) {
+		pmemstream_delete(&stream);
+	};
+	return std::unique_ptr<struct pmemstream, decltype(stream_delete)>(stream, std::move(stream_delete));
+}
 
 #endif /* PMEMSTREAM_UNITTEST_HPP */
---
+++ tests/unittest/append.cpp
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+#include <algorithm>
+#include <bitset>
+#include <cstring>
+#include <numeric>
+#include <vector>
+
+#include <rapidcheck.h>
+
+#include "unittest.hpp"
+
+static constexpr size_t stream_size = 1024 * 1024;
+
+namespace
+{
+void append_at_offset(struct pmemstream *stream, struct pmemstream_region *region, struct pmemstream_entry *offset,
+		      const std::vector<std::string> &data)
+{
+	for (const auto &e : data) {
+		RC_ASSERT(pmemstream_append(stream, region, offset, e.data(), e.size(), nullptr) == 0);
+	}
+}
+
+struct pmemstream_region initialize_stream_single_region(struct pmemstream *stream, size_t region_size,
+							 const std::vector<std::string> &data)
+{
+	struct pmemstream_region new_region;
+	RC_ASSERT(pmemstream_region_allocate(stream, region_size, &new_region) == 0);
+
+	struct pmemstream_entry_iterator *eiter;
+	RC_ASSERT(pmemstream_entry_iterator_new(&eiter, stream, new_region) == 0);
+
+	/* Find out offset for the first entry in region */
+	struct pmemstream_entry entry;
+	RC_ASSERT(pmemstream_entry_iterator_next(eiter, NULL, &entry) == -1);
+	pmemstream_entry_iterator_delete(&eiter);
+
+	append_at_offset(stream, &new_region, &entry, data);
+
+	return new_region;
+}
+
+std::vector<std::string> get_elements_in_region(struct pmemstream *stream, struct pmemstream_region *region)
+{
+	std::vector<std::string> result;
+
+	struct pmemstream_entry_iterator *eiter;
+	RC_ASSERT(pmemstream_entry_iterator_new(&eiter, stream, *region) == 0);
+
+	struct pmemstream_entry entry;
+	while (pmemstream_entry_iterator_next(eiter, NULL, &entry) == 0) {
+		auto data_ptr = reinterpret_cast<char *>(pmemstream_entry_data(stream, entry));
+		result.emplace_back(data_ptr, pmemstream_entry_length(stream, entry));
+	}
+
+	pmemstream_entry_iterator_delete(&eiter);
+
+	return result;
+}
+
+struct pmemstream_entry get_append_offset(struct pmemstream *stream, struct pmemstream_region *region)
+{
+	struct pmemstream_entry_iterator *eiter;
+	RC_ASSERT(pmemstream_entry_iterator_new(&eiter, stream, *region) == 0);
+
+	struct pmemstream_entry entry;
+	while (pmemstream_entry_iterator_next(eiter, NULL, &entry) == 0) {
+		/* do nothing */
+	}
+
+	pmemstream_entry_iterator_delete(&eiter);
+
+	return entry;
+}
+} // namespace
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " file" << std::endl;
+		return -1;
+	}
+
+	auto file = std::string(argv[1]);
+
+	return run_test([&] {
+		return_check ret;
+
+		ret += rc::check("verify popcount against custom implementation",
+				 [&](const std::vector<std::string> &data, const std::vector<std::string> &extra_data) {
+					 static constexpr size_t region_size = 4096;
+					 static constexpr size_t block_size = 4096;
+					 auto stream = make_pmemstream(file, block_size, stream_size);
+
+					 /* Allocate region and init it with data */
+					 auto region = initialize_stream_single_region(stream.get(), region_size, data);
+
+					 /* Verify that all data matches */
+					 RC_ASSERT(get_elements_in_region(stream.get(), &region) == data);
+
+					 /* Find out offset at which we can append */
+					 auto append_offset = get_append_offset(stream.get(), &region);
+					 /* Append extra_data to the end */
+					 append_at_offset(stream.get(), &region, &append_offset, extra_data);
+
+					 /* Verify if stream now holds data + extra_data */
+					 auto all_elements = get_elements_in_region(stream.get(), &region);
+					 auto extra_data_start = all_elements.begin() + static_cast<int>(data.size());
+
+					 RC_ASSERT(std::equal(all_elements.begin(), extra_data_start, data.begin()));
+					 RC_ASSERT(
+						 std::equal(extra_data_start, all_elements.end(), extra_data.begin()));
+				 });
+	});
+}
