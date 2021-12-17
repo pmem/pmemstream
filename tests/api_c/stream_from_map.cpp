bafcebfec6119ca7f9e6b981fbaa5b1c089890c2
Igor Chorążewicz <igor.chorazewicz@intel.com>
2021-12-17 11:28:16 +0100

    Add truncate flag to map_open.
    It will be useful for testing pmemstream after reopen.
    
    Also, remove run_with_file.cmake which is not necessary - we can just
    pass truncate flag to open.

--- tests/api_c/stream_from_map.cpp
+++ tests/api_c/stream_from_map.cpp
@@ -11,7 +11,7 @@
 
 void test_stream_from_map(char *path, size_t file_size, size_t blk_size)
 {
-	struct pmem2_map *map = map_open(path, file_size);
+	struct pmem2_map *map = map_open(path, file_size, true);
 	UT_ASSERTne(map, NULL);
 
 	struct pmemstream *s = NULL;
