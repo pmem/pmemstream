19a28a129fd0484cb01535e49485265e618049f1
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2021-12-25 20:53:51 -0500

    Add critnib to cppstyle and check_whitespace

--- src/critnib/critnib.h
+++ src/critnib/critnib.h
@@ -14,11 +14,11 @@ struct critnib;
 typedef struct critnib critnib;
 
 enum find_dir_t {
-	FIND_L  = -2,
+	FIND_L = -2,
 	FIND_LE = -1,
-	FIND_EQ =  0,
+	FIND_EQ = 0,
 	FIND_GE = +1,
-	FIND_G  = +2,
+	FIND_G = +2,
 };
 
 critnib *critnib_new(void);
@@ -28,10 +28,9 @@ int critnib_insert(critnib *c, uintptr_t key, void *value, int update);
 void *critnib_remove(critnib *c, uintptr_t key);
 void *critnib_get(critnib *c, uintptr_t key);
 void *critnib_find_le(critnib *c, uintptr_t key);
-int critnib_find(critnib *c, uintptr_t key, enum find_dir_t dir,
-	uintptr_t *rkey, void **rvalue);
-void critnib_iter(critnib *c, uintptr_t min, uintptr_t max,
-       int (*func)(uintptr_t key, void *value, void *privdata), void *privdata);
+int critnib_find(critnib *c, uintptr_t key, enum find_dir_t dir, uintptr_t *rkey, void **rvalue);
+void critnib_iter(critnib *c, uintptr_t min, uintptr_t max, int (*func)(uintptr_t key, void *value, void *privdata),
+		  void *privdata);
 
 #ifdef __cplusplus
 }
