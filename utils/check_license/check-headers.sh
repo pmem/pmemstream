6cef2d676743b28ee4c2b8d59738e81f6d730f23
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-01-05 12:47:17 +0100

    utils: remove too verbose warning in check-heders.sh
    it looks like a very important warning, expanding over few lines of
    output, but in reality we all know what we do - just check
    modified files instead of the whole repository ;-)

--- utils/check_license/check-headers.sh
+++ utils/check_license/check-headers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2021, Intel Corporation
+# Copyright 2016-2022, Intel Corporation
 
 # check-headers.sh - check copyright and license in source files
 
@@ -76,18 +76,10 @@ if [ $CHECK_ALL -eq 0 ]; then
 fi
 
 if [ $CHECK_ALL -eq 1 ]; then
-	echo "Checking copyright headers of all files..."
+	echo "INFO: Checking copyright headers of all files..."
 	GIT_COMMAND="ls-tree -r --name-only HEAD"
 else
-	if [ $VERBOSE -eq 1 ]; then
-		echo
-		echo "Warning: will check copyright headers of modified files only,"
-		echo "         in order to check all files issue the following command:"
-		echo "         $ $SELF <source_root_path> <license_tag> -a"
-		echo "         (e.g.: $ $SELF $SOURCE_ROOT $LICENSE -a)"
-		echo
-	fi
-	echo "Checking copyright headers of modified files only..."
+	echo "INFO: Checking copyright headers of modified files only..."
 	GIT_COMMAND="diff --name-only $MERGE_BASE $CURRENT_COMMIT"
 fi
 
