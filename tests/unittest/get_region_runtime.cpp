2826645c21b6ad845d952399da92e7cb154e9c3d
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-01-10 19:12:28 +0100

    Rename region_context to region_runtime
    Also rename functions responsible for initializing append offset
    to contain runtime_initialize. This creates better abstraction and
    allows us to do more things in region_initialize than just initializing
    offset (without need to change name in the future).

--- tests/unittest/get_region_runtime.cpp
+++ tests/unittest/get_region_runtime.cpp
@@ -24,15 +24,15 @@ int main(int argc, char *argv[])
 	return run_test([&] {
 		return_check ret;
 
-		ret += rc::check("verify pmemstream_get_region_context return the same value for all threads", [&]() {
+		ret += rc::check("verify pmemstream_get_region_runtime return the same value for all threads", [&]() {
 			const auto concurrency = *rc::gen::inRange<std::size_t>(0, max_concurrency);
 
 			auto stream = make_pmemstream(path, TEST_DEFAULT_BLOCK_SIZE, TEST_DEFAULT_STREAM_SIZE);
 			auto region = initialize_stream_single_region(stream.get(), TEST_DEFAULT_REGION_SIZE, {});
 
-			std::vector<pmemstream_region_context *> threads_data(concurrency);
+			std::vector<pmemstream_region_runtime *> threads_data(concurrency);
 			parallel_exec(concurrency, [&](size_t tid) {
-				RC_ASSERT(pmemstream_get_region_context(stream.get(), region, &threads_data[tid]) == 0);
+				RC_ASSERT(pmemstream_get_region_runtime(stream.get(), region, &threads_data[tid]) == 0);
 
 				auto is_nullptr = threads_data[tid] == nullptr;
 				RC_ASSERT(!is_nullptr);
