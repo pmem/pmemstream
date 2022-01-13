28126316454130165e09b77e883ba5d1d1e4c5bc
Karolina Bober <karolina.bober@intel.com>
2022-01-13 17:52:11 +0100

    Test public API functions

--- tests/api_c/append_entry.c
+++ tests/api_c/append_entry.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright 2021-2022, Intel Corporation */
 
+#include "span.h"
 #include "unittest.h"
 
 /**
@@ -12,7 +13,7 @@ struct entry_data {
 	uint64_t data;
 };
 
-void test_entry_iterator(char *path)
+void valid_input_test(char *path)
 {
 	int ret;
 	struct entry_data data;
@@ -43,6 +44,97 @@ void test_entry_iterator(char *path)
 	pmem2_map_delete(&map);
 }
 
+void invalid_region_test(char *path)
+{
+	int ret;
+	struct entry_data data;
+	data.data = UINT64_MAX;
+	struct pmemstream_entry *entry = NULL;
+
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region = {.offset = ALIGN_DOWN(UINT64_MAX, sizeof(span_bytes))};
+
+	ret = pmemstream_append(stream, region, NULL, &data, sizeof(data), entry);
+	UT_ASSERTeq(ret, -1);
+	UT_ASSERTeq(&entry, NULL);
+
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void null_data_test(char *path)
+{
+	int ret;
+	struct pmemstream_entry *entry = NULL;
+
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+
+	ret = pmemstream_append(stream, region, NULL, NULL, 0, entry);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTeq(entry, NULL);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void null_entry_test(char *path)
+{
+	int ret;
+	struct entry_data data;
+	data.data = UINT64_MAX;
+	struct pmemstream_entry *entry = NULL;
+
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	ret = pmemstream_append(stream, region, NULL, &data, sizeof(data), entry);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTeq(entry, NULL);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void invalid_entry_test(char *path)
+{
+	int ret;
+	const struct entry_data *entry_data;
+	struct pmemstream_entry entry = {.offset = ALIGN_DOWN(UINT64_MAX, sizeof(span_bytes))};
+
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	entry_data = pmemstream_entry_data(stream, entry);
+	UT_ASSERTeq(entry_data, NULL);
+
+	UT_ASSERTeq(pmemstream_entry_length(stream, entry), 0);
+
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
@@ -53,7 +145,11 @@ int main(int argc, char *argv[])
 
 	char *path = argv[1];
 
-	test_entry_iterator(path);
+	valid_input_test(path);
+	invalid_region_test(path);
+	null_data_test(path);
+	null_entry_test(path);
+	invalid_entry_test(path);
 
 	return 0;
 }
--- tests/api_c/reserve_and_publish.c
+++ tests/api_c/reserve_and_publish.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright 2021-2022, Intel Corporation */
 
+#include "span.h"
 #include "unittest.h"
 #include <string.h>
 
@@ -12,7 +13,7 @@ struct entry_data {
 	uint64_t data;
 };
 
-void test_reserve_and_publish(char *path)
+void valid_input_test(char *path)
 {
 	int ret;
 	void *data_address = NULL;
@@ -39,7 +40,7 @@ void test_reserve_and_publish(char *path)
 	pmem2_map_delete(&map);
 }
 
-void test_reserve_and_publish_with_memcpy(char *path)
+void valid_input_test_with_memcpy(char *path)
 {
 	int ret;
 	void *data_address = NULL;
@@ -68,6 +69,83 @@ void test_reserve_and_publish_with_memcpy(char *path)
 	pmem2_map_delete(&map);
 }
 
+void invalid_region_test(char *path)
+{
+	int ret;
+	void *data_address = NULL;
+	struct entry_data data;
+	struct pmemstream_entry entry;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region = {.offset = ALIGN_DOWN(UINT64_MAX, sizeof(span_bytes))};
+
+	ret = pmemstream_reserve(stream, region, NULL, sizeof(data), &entry, &data_address);
+	UT_ASSERTeq(ret, -1);
+	UT_ASSERTeq(data_address, NULL);
+
+	ret = pmemstream_publish(stream, region, NULL, &data, sizeof(data), &entry);
+	UT_ASSERTeq(ret, -1);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void null_data_test(char *path)
+{
+	int ret;
+	void *data_address = NULL;
+	struct entry_data *data = NULL;
+	struct pmemstream_entry entry;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	ret = pmemstream_reserve(stream, region, NULL, 0, &entry, &data_address);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(data_address, NULL);
+
+	ret = pmemstream_publish(stream, region, NULL, data, 0, &entry);
+	UT_ASSERTeq(ret, 0);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void zero_size_test(char *path)
+{
+	int ret;
+	void *data_address = NULL;
+	struct pmemstream_entry entry;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	ret = pmemstream_reserve(stream, region, NULL, 0, &entry, &data_address);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(data_address, NULL);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
@@ -78,8 +156,11 @@ int main(int argc, char *argv[])
 
 	char *path = argv[1];
 
-	test_reserve_and_publish(path);
-	test_reserve_and_publish_with_memcpy(path);
+	valid_input_test(path);
+	valid_input_test_with_memcpy(path);
+	invalid_region_test(path);
+	null_data_test(path);
+	zero_size_test(path);
 
 	return 0;
 }
