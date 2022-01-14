3fa13ca49d67cdca542e9b0f98cf8e061b8cdca7
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-01-14 18:41:10 +0100

    Add concurrent_iterate_with_append test and fix iterator
    Iterator fix is to call span_get_runtime only if region is initialized.
    This ensure that there is no race between append (and setting span
    type) and iterators reading span type.

---
+++ tests/unittest/concurrent_iterate_with_append.cpp
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+#include <vector>
+
+#include <rapidcheck.h>
+
+#include "stream_helpers.hpp"
+#include "thread_helpers.hpp"
+#include "unittest.hpp"
+
+static constexpr size_t concurrency = 4;
+static constexpr size_t max_entry_size = 1024;
+
+namespace
+{
+void concurrent_iterate_verify(pmemstream *stream, pmemstream_region region, const std::vector<std::string> &data,
+			       const std::vector<std::string> &extra_data)
+{
+	std::vector<std::string> result;
+
+	struct pmemstream_entry_iterator *eiter;
+	RC_ASSERT(pmemstream_entry_iterator_new(&eiter, stream, region) == 0);
+
+	struct pmemstream_entry entry;
+
+	/* Loop until all entires are found. */
+	while (result.size() < data.size() + extra_data.size()) {
+		int next = pmemstream_entry_iterator_next(eiter, NULL, &entry);
+		if (next == 0) {
+			auto data_ptr = reinterpret_cast<char *>(pmemstream_entry_data(stream, entry));
+			result.emplace_back(data_ptr, pmemstream_entry_length(stream, entry));
+		}
+	}
+
+	RC_ASSERT(std::equal(data.begin(), data.end(), result.begin()));
+	RC_ASSERT(
+		std::equal(extra_data.begin(), extra_data.end(), result.begin() + static_cast<long long>(data.size())));
+
+	pmemstream_entry_iterator_delete(&eiter);
+}
+} // namespace
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " path" << std::endl;
+		return -1;
+	}
+
+	auto path = std::string(argv[1]);
+
+	return run_test([&] {
+		return_check ret;
+
+		/* Disable shrinking and set max_size of entries. */
+		/* XXX: can we do though rapidcheck API? */
+		std::string rapidcheck_config =
+			"RC_PARAMS=\"noshrink=1\" max_size=\"" + std::to_string(max_entry_size) + "\"";
+		int r = putenv(rapidcheck_config.data());
+		UT_ASSERT(r == 0);
+
+		ret += rc::check(
+			"verify if iterators concurrent to append work do not return garbage (no preinitialization)",
+			[&](std::vector<std::string> &&extra_data, bool use_region_runtime) {
+				auto stream = make_pmemstream(path, TEST_DEFAULT_BLOCK_SIZE, TEST_DEFAULT_STREAM_SIZE);
+				auto region =
+					initialize_stream_single_region(stream.get(), TEST_DEFAULT_REGION_SIZE, {});
+
+				parallel_exec(concurrency, [&](size_t tid) {
+					if (tid == 0) {
+						/* appender */
+						pmemstream_region_runtime *region_runtime = nullptr;
+						if (use_region_runtime) {
+							pmemstream_get_region_runtime(stream.get(), region,
+										      &region_runtime);
+						}
+						append(stream.get(), region, region_runtime, extra_data);
+						verify(stream.get(), region, extra_data, {});
+					} else {
+						/* iterators */
+						concurrent_iterate_verify(stream.get(), region, extra_data, {});
+					}
+				});
+			});
+
+		ret += rc::check(
+			"verify if iterators concurrent to append work do not return garbage",
+			[&](const std::vector<std::string> &data, const std::vector<std::string> &extra_data,
+			    bool use_region_runtime) {
+				auto stream = make_pmemstream(path, TEST_DEFAULT_BLOCK_SIZE, TEST_DEFAULT_STREAM_SIZE);
+				auto region =
+					initialize_stream_single_region(stream.get(), TEST_DEFAULT_REGION_SIZE, data);
+
+				parallel_exec(concurrency, [&](size_t tid) {
+					if (tid == 0) {
+						/* appender */
+						pmemstream_region_runtime *region_runtime = nullptr;
+						if (use_region_runtime) {
+							pmemstream_get_region_runtime(stream.get(), region,
+										      &region_runtime);
+						}
+						append(stream.get(), region, region_runtime, extra_data);
+						verify(stream.get(), region, data, extra_data);
+					} else {
+						/* iterators */
+						concurrent_iterate_verify(stream.get(), region, data, extra_data);
+					}
+				});
+			});
+
+		ret += rc::check(
+			"verify if iterators concurrent to append work do not return garbage after reopen",
+			[&](const std::vector<std::string> &data, const std::vector<std::string> &extra_data,
+			    bool use_region_runtime) {
+				pmemstream_region region;
+				{
+					auto stream = make_pmemstream(path, TEST_DEFAULT_BLOCK_SIZE,
+								      TEST_DEFAULT_STREAM_SIZE);
+					region = initialize_stream_single_region(stream.get(), TEST_DEFAULT_REGION_SIZE,
+										 data);
+				}
+
+				auto stream =
+					make_pmemstream(path, TEST_DEFAULT_BLOCK_SIZE, TEST_DEFAULT_STREAM_SIZE, false);
+				std::vector parallel_exec(concurrency, [&](size_t tid) {
+					if (tid == 0) {
+						/* appender */
+						pmemstream_region_runtime *region_runtime = nullptr;
+						if (use_region_runtime) {
+							pmemstream_get_region_runtime(stream.get(), region,
+										      &region_runtime);
+						}
+						append(stream.get(), region, region_runtime, extra_data);
+						verify(stream.get(), region, data, extra_data);
+					} else {
+						/* iterators */
+						concurrent_iterate_verify(stream.get(), region, data, extra_data);
+					}
+				});
+			});
+	});
+}
