cb69c8ce0f9515b0252fe95a939da2c7f8e08014
Igor Chorążewicz <igor.chorazewicz@intel.com>
2022-01-17 14:07:15 +0100

    Implement helper test function
    For wrapping pointers to C structs (with ctor and dtor functions)
    into unique_ptr.

--- tests/common/unittest.hpp
+++ tests/common/unittest.hpp
@@ -125,4 +125,14 @@ auto make_pmemstream(const std::string &file, size_t block_size, size_t size, bo
 	return std::unique_ptr<struct pmemstream, decltype(stream_delete)>(stream, std::move(stream_delete));
 }
 
+/* Return function which constructs (creates unique_ptr) an instance of an object using ctor().
+ * It's main purpose is to wrap C-like interface with _new and _destroy functions in unique_ptr. */
+template <typename Ctor, typename Dtor>
+auto make_instance_ctor(Ctor &&ctor, Dtor &&dtor)
+{
+	return [&] {
+		return std::unique_ptr<std::remove_reference_t<decltype(*ctor())>, decltype(&dtor)>(ctor(), &dtor);
+	};
+}
+
 #endif /* LIBPMEMSTREAM_UNITTEST_HPP */
