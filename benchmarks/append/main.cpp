57bb310a5f444766a06163f25c98e14243bfadf3
Krzysztof Filipek <krzysztof.filipek@intel.com>
2022-01-21 14:50:04 +0100

    Add pmemlog engine for benchmark-append

--- benchmarks/append/main.cpp
+++ benchmarks/append/main.cpp
@@ -2,20 +2,29 @@
 /* Copyright 2022, Intel Corporation */
 
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <cstdlib>
+#include <forward_list>
 #include <getopt.h>
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
+#include <string_view>
 #include <vector>
 
 #include "measure.hpp"
 /* XXX: Change this header when make_pmemstream moved to public API */
 #include "unittest.hpp"
 
-class Config {
+#include <libpmemlog.h>
+
+class config {
  private:
-	static constexpr option long_options[] = {{"path", required_argument, NULL, 'p'},
-						  {"stream_size", required_argument, NULL, 'x'},
+	static constexpr std::array<std::string_view, 2> engine_names = {"pmemlog", "pmemstream"};
+	static constexpr option long_options[] = {{"engine", required_argument, NULL, 'e'},
+						  {"path", required_argument, NULL, 'p'},
+						  {"size", required_argument, NULL, 'x'},
 						  {"block_size", required_argument, NULL, 'b'},
 						  {"region_size", required_argument, NULL, 'r'},
 						  {"element_count", required_argument, NULL, 'c'},
@@ -25,11 +34,37 @@ class Config {
 						  {"help", no_argument, NULL, 'h'},
 						  {NULL, 0, NULL, 0}};
 
-	std::string app_name;
+	static std::string app_name;
+
+	static std::string available_engines()
+	{
+		std::string possible_engines;
+		for (auto name : engine_names) {
+			possible_engines += std::string(name) + " ";
+		}
+		return possible_engines;
+	}
+
+	void set_engine(std::string engine_name)
+	{
+		engine = engine_name;
+		if (std::find(engine_names.begin(), engine_names.end(), engine) == engine_names.end()) {
+			throw std::invalid_argument(std::string("Wrong engine name, possible: ") + available_engines());
+		}
+	}
+
+	void set_size(size_t size_arg)
+	{
+		if (size_arg < size) {
+			throw std::invalid_argument(std::string("Invalid size, should be >=") + std::to_string(size));
+		}
+		size = size_arg;
+	}
 
  public:
+	std::string engine = "pmemstream";
 	std::string path;
-	size_t stream_size = TEST_DEFAULT_STREAM_SIZE;
+	size_t size = (PMEMLOG_MIN_POOL > TEST_DEFAULT_STREAM_SIZE ? PMEMLOG_MIN_POOL : TEST_DEFAULT_STREAM_SIZE);
 	size_t block_size = TEST_DEFAULT_BLOCK_SIZE;
 	size_t region_size = TEST_DEFAULT_REGION_SIZE;
 	size_t element_count = 100000;
@@ -41,13 +76,16 @@ class Config {
 	{
 		app_name = std::string(argv[0]);
 		int ch;
-		while ((ch = getopt_long(argc, argv, "p:x:b:r:c:s:i:nh", long_options, NULL)) != -1) {
+		while ((ch = getopt_long(argc, argv, "e:p:x:b:r:c:s:i:nh", long_options, NULL)) != -1) {
 			switch (ch) {
+				case 'e':
+					set_engine(std::string(optarg));
+					break;
 				case 'p':
 					path = std::string(optarg);
 					break;
 				case 'x':
-					stream_size = std::stoull(optarg);
+					set_size(std::stoull(optarg));
 					break;
 				case 'b':
 					block_size = std::stoull(optarg);
@@ -79,82 +117,177 @@ class Config {
 		return 0;
 	}
 
-	std::string usage()
+	static void print_usage()
 	{
-		return "Usage:" + app_name + "[OPTION]...\n" + "Pmemstream benchmark for append\n" +
-			"\n\t--path [path] path to file" + "\n\t--stream_size [size] stream size" +
-			"\n\t--block size [size] block size" + "\n\t--region_size [size] region size" +
-			"\n\t--element_count [count] Number of elements inserted into stream" +
-			"\n\t--element_size [size] Number of bytes of each element inserted into stream" +
-			"\n\t--iterations [iterations] Number of iterations. " +
-			"More iterations gives more robust statistical data, but takes more time" +
-			"\n\t--null_region_runtime indicates if **null** region runtime would be passed to append" +
-			"\n\t--help display this message";
+		std::vector<std::string> new_line = {"", ""};
+		std::vector<std::vector<std::string>> options = {
+			{"Usage: " + app_name + " [OPTION]...\n" + "Log-like structure benchmark for append.", ""},
+			new_line,
+			{"--engine [name]", "engine name, possible values: " + available_engines()},
+			{"--path [path]", "path to file"},
+			{"--size [size]", "log size"},
+			{"--element_count [count]", "number of elements to be inserted"},
+			{"--element_size [size]", "number of bytes of each element"},
+			{"--iterations [iterations]", "number of iterations. "},
+			new_line,
+			{"pmemstream related options:", ""},
+			{"--block_size [size]", "block size"},
+			{"--region_size [size]", "region size"},
+			new_line,
+			{"More iterations gives more robust statistical data, but takes more time", ""},
+			{"--null_region_runtime", "indicates if **null** region runtime would be passed to append"},
+			{"--help", "display this message"}};
+		for (auto &option : options) {
+			std::cout << std::setw(25) << std::left << option[0] << " " << option[1] << std::endl;
+		}
 	}
 };
-constexpr option Config::long_options[];
+std::string config::app_name;
+constexpr option config::long_options[];
+constexpr std::array<std::string_view, 2> config::engine_names;
 
-std::ostream &operator<<(std::ostream &out, Config const &cfg)
+std::ostream &operator<<(std::ostream &out, config const &cfg)
 {
-	out << "Pmemstream Benchmark, path: " << cfg.path << ", ";
-	out << "stream_size: " << cfg.stream_size << ", ";
+	out << "Log-like structure Benchmark, path: " << cfg.path << ", ";
+	out << "size: " << cfg.size << ", ";
 	out << "block_size: " << cfg.block_size << ", ";
 	out << "region_size: " << cfg.region_size << ", ";
 	out << "element_count: " << cfg.element_count << ", ";
 	out << "element_size: " << cfg.element_size << ", ";
 	out << "null_region_runtime: " << std::boolalpha << cfg.null_region_runtime << ", ";
-	out << "Number of iterations: " << cfg.iterations;
+	out << "Number of iterations: " << cfg.iterations << std::endl;
 	return out;
 }
+
+class workload_base {
+ public:
+	virtual ~workload_base(){};
+	virtual void perform() = 0;
+	virtual int initialize() = 0;
+	void prepareData(size_t bytes_to_generate)
+	{
+		auto input_generation_time = benchmark::measure<std::chrono::seconds>(
+			[&] { data = benchmark::generate_data(bytes_to_generate); });
+		data_chunks = reinterpret_cast<uint8_t *>(data.data());
+		std::cout << "input generation time: " << input_generation_time << "s" << std::endl;
+	}
+	uint8_t *data_chunks;
+	std::vector<uint64_t> data;
+};
+
+class pmemlog_workload : public workload_base {
+ private:
+	config cfg;
+	PMEMlogpool *plp;
+
+ public:
+	pmemlog_workload(config &cfg) : cfg(cfg)
+	{
+	}
+
+	virtual void perform() override
+	{
+		for (auto it = data.begin(); it != data.end(); std::advance(it, cfg.element_size)) {
+			if (pmemlog_append(plp, &it, cfg.element_size) < 0) {
+				throw std::runtime_error("Error while appending new entry!");
+			}
+		}
+	}
+
+	virtual int initialize() override
+	{
+		auto path = cfg.path.c_str();
+		plp = pmemlog_create(path, cfg.size, 0666);
+		if (plp == NULL)
+			plp = pmemlog_open(path);
+		if (plp == NULL) {
+			perror(path);
+			// maybe throw?
+			return -1;
+		}
+		auto bytes_to_generate = cfg.element_count * cfg.element_size;
+		prepareData(bytes_to_generate);
+		return 0;
+	}
+};
+
+class pmemstream_workload : public workload_base {
+ private:
+	config cfg;
+	struct pmemstream_region region;
+	pmemstream_region_runtime *region_runtime_ptr;
+	std::unique_ptr<struct pmemstream, std::function<void(struct pmemstream *)>> stream;
+
+ public:
+	pmemstream_workload(config &cfg) : cfg(cfg)
+	{
+		stream = make_pmemstream(cfg.path.c_str(), cfg.block_size, cfg.size);
+	}
+
+	virtual int initialize() override
+	{
+		if (pmemstream_region_allocate(stream.get(), cfg.region_size, &region)) {
+			return -1;
+		}
+		if (!cfg.null_region_runtime &&
+		    pmemstream_get_region_runtime(stream.get(), region, &region_runtime_ptr)) {
+			return -2;
+		}
+
+		auto bytes_to_generate = cfg.element_count * cfg.element_size;
+		prepareData(bytes_to_generate);
+
+		return 0;
+	}
+
+	void perform() override
+	{
+		for (size_t i = 0; i < data.size() * sizeof(uint64_t); i += cfg.element_size) {
+			if (pmemstream_append(stream.get(), region, region_runtime_ptr, data_chunks + i,
+					      cfg.element_size, NULL) < 0) {
+				throw std::runtime_error("Error while appending new entry!");
+			}
+		}
+	}
+};
 
 int main(int argc, char *argv[])
 {
 	// auto config = parse_arguments(argc, argv);
-	Config config;
+	config cfg;
 	try {
-		if (config.parse_arguments(argc, argv) != 0) {
-			std::cout << config.usage() << std::endl;
+		if (cfg.parse_arguments(argc, argv) != 0) {
+			config::print_usage();
 			exit(0);
 		}
 	} catch (std::invalid_argument const &e) {
 		std::cerr << e.what() << std::endl;
 		exit(1);
 	}
-	std::cout << config << std::endl;
+	std::cout << cfg << std::endl;
 
-	auto bytes_to_generate = config.element_count * config.element_size;
+	workload_base *workload = nullptr;
 
-	std::vector<uint64_t> data;
-	auto input_generation_time =
-		benchmark::measure<std::chrono::seconds>([&] { data = benchmark::generate_data(bytes_to_generate); });
-
-	std::cout << "input generation time: " << input_generation_time << "s" << std::endl;
-
-	auto stream = make_pmemstream(config.path.c_str(), config.block_size, config.stream_size);
-	struct pmemstream_region region;
-	if (pmemstream_region_allocate(stream.get(), config.region_size, &region)) {
-		return -1;
+	if (cfg.engine == "pmemlog") {
+		workload = new pmemlog_workload(cfg);
+	} else if (cfg.engine == "pmemstream") {
+		workload = new pmemstream_workload(cfg);
 	}
 
-	pmemstream_region_runtime *region_runtime_ptr = NULL;
-	if (!config.null_region_runtime) {
-		if (pmemstream_get_region_runtime(stream.get(), region, &region_runtime_ptr)) {
-			return -2;
-		}
+	if (workload->initialize() != 0) {
+		std::cerr << "Error during initialization..." << std::endl;
+		exit(2);
 	}
-	auto data_chunks = reinterpret_cast<uint8_t *>(data.data());
 	/* XXX: Add initialization phase whith separate measurement */
-	auto results = benchmark::measure<std::chrono::nanoseconds>(config.iterations, [&] {
-		for (size_t i = 0; i < data.size() * sizeof(uint64_t); i += config.element_size) {
-			pmemstream_append(stream.get(), region, region_runtime_ptr, data_chunks + i,
-					  config.element_size, NULL);
-		}
-	});
+	auto results =
+		benchmark::measure<std::chrono::nanoseconds>(cfg.iterations, [workload] { workload->perform(); });
 
-	auto mean = benchmark::mean(results) / config.element_count;
-	auto max = static_cast<size_t>(benchmark::max(results)) / config.element_count;
-	auto min = static_cast<size_t>(benchmark::min(results)) / config.element_count;
-	auto std_dev = benchmark::std_dev(results) / config.element_count;
+	delete workload;
+
+	auto mean = benchmark::mean(results) / cfg.element_count;
+	auto max = static_cast<size_t>(benchmark::max(results)) / cfg.element_count;
+	auto min = static_cast<size_t>(benchmark::min(results)) / cfg.element_count;
+	auto std_dev = benchmark::std_dev(results) / cfg.element_count;
 
 	std::cout << "pmemstream_append measurement:" << std::endl;
 	std::cout << "\tmean: " << mean << "ns" << std::endl;
