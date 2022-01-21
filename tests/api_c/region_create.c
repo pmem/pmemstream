fc332c348b7c9ef2f5b8f7eeee11cba692d104b9
Karolina Bober <karolina.bober@intel.com>
2022-01-21 18:28:01 +0100

    Test public API functions
    Pass only valid arguments

---
+++ tests/api_c/region_create.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021-2022, Intel Corporation */
+
+#include "unittest.h"
+
+/**
+ * region_create - unit test for pmemstream_region_allocate, pmemstream_region_free,
+ *					pmemstream_region_size, pmemstream_get_region_runtime
+ */
+
+void test_region_create(char *path)
+{
+	int ret;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	size_t region_size;
+	region_size = pmemstream_region_size(stream, region);
+	UT_ASSERT(region_size >= TEST_DEFAULT_REGION_SIZE);
+
+	struct pmemstream_region_runtime *rtm = NULL;
+	ret = pmemstream_get_region_runtime(stream, region, &rtm);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(rtm, NULL);
+
+	ret = pmemstream_region_free(stream, region);
+	UT_ASSERTeq(ret, 0);
+
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	START();
+	char *path = argv[1];
+
+	test_region_create(path);
+
+	return 0;
+}
---
+++ tests/api_c/reserve_and_publish.c
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021-2022, Intel Corporation */
+
+#include "unittest.h"
+#include <string.h>
+
+/**
+ * reserve_and_publish - unit test for pmemstream_reserve, pmemstream_publish
+ */
+
+struct entry_data {
+	uint64_t data;
+};
+
+void test_reserve_and_publish(char *path)
+{
+	int ret;
+	void *data_address = NULL;
+	struct entry_data data;
+	struct pmemstream_entry entry;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	ret = pmemstream_reserve(stream, region, NULL, sizeof(data), &entry, &data_address);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(data_address, NULL);
+
+	ret = pmemstream_publish(stream, region, NULL, &data, sizeof(data), &entry);
+	UT_ASSERTeq(ret, 0);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+void test_reserve_and_publish_with_memcpy(char *path)
+{
+	int ret;
+	void *data_address = NULL;
+	struct entry_data data, data_mcpy;
+	struct pmemstream_entry entry;
+	struct pmem2_map *map = map_open(path, TEST_DEFAULT_STREAM_SIZE, true);
+	struct pmemstream *stream;
+	ret = pmemstream_from_map(&stream, TEST_DEFAULT_BLOCK_SIZE, map);
+	UT_ASSERTeq(ret, 0);
+
+	struct pmemstream_region region;
+	ret = pmemstream_region_allocate(stream, TEST_DEFAULT_REGION_SIZE, &region);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(&region, NULL);
+
+	ret = pmemstream_reserve(stream, region, NULL, sizeof(data), &entry, &data_address);
+	UT_ASSERTeq(ret, 0);
+	UT_ASSERTne(data_address, NULL);
+
+	memcpy(&data_address, &data_mcpy, sizeof(data));
+
+	ret = pmemstream_publish(stream, region, NULL, &data, sizeof(data), &entry);
+	UT_ASSERTeq(ret, 0);
+
+	pmemstream_region_free(stream, region);
+	pmemstream_delete(&stream);
+	pmem2_map_delete(&map);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	START();
+
+	char *path = argv[1];
+
+	test_reserve_and_publish(path);
+	test_reserve_and_publish_with_memcpy(path);
+
+	return 0;
+}
