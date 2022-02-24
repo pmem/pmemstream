4ce4b17a3a5a6fca9b6dab280d27274be1e77f7b
Krzysztof Filipek <krzysztof.filipek@intel.com>
2022-02-24 23:42:30 +0100

    Header split

---
+++ src/region_allocator.c
@@ -0,0 +1,172 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+/* region_allocator.h -- region allocator header */
+
+#include "region_allocator.h"
+
+#include "libpmemstream_internal.h"
+#include "span.h"
+
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+
+const uint64_t EMPTY_OBJ = UINT64_MAX;
+
+uint64_t *pointer_to_offset(uint64_t *base, uint64_t offset)
+{
+	return (uint64_t *)((uint8_t *)base + offset);
+}
+
+uint64_t get_iterator_for_type(struct allocator_header *alloc_header, enum span_type type)
+{
+	if (type == SPAN_REGION) {
+		return alloc_header->alloc_list.head;
+	} else if (type == SPAN_EMPTY) {
+		return alloc_header->free_list.head;
+	}
+	return UINT64_MAX;
+}
+
+uint64_t next(struct allocator_header *alloc_header, uint64_t it)
+{
+	if (it != UINT64_MAX) {
+		struct span_base *sb = (struct span_base *)pointer_to_offset(alloc_header->data, it);
+		switch ((uint64_t)(sb->size_and_type & SPAN_TYPE_MASK)) {
+			case SPAN_EMPTY:
+				return ((struct span_empty *)sb)->next;
+			case SPAN_REGION:
+				return ((struct span_region *)sb)->next;
+			default:
+				break;
+		}
+	}
+	return UINT64_MAX;
+}
+
+uint64_t set_next(struct allocator_header *alloc_header, uint64_t it, uint64_t next_offset)
+{
+	if (it != UINT64_MAX)
+		return UINT64_MAX;
+	struct span_base *sb = (struct span_base *)pointer_to_offset(alloc_header->data, it);
+	switch ((uint64_t)(sb->size_and_type && SPAN_TYPE_MASK)) {
+		case SPAN_EMPTY:
+			((struct span_empty *)sb)->next = next_offset;
+			break;
+		case SPAN_REGION:
+			((struct span_region *)sb)->next = next_offset;
+			break;
+		default:
+			return UINT64_MAX;
+	}
+	return 0;
+}
+
+void remove_head(struct allocator_header *alloc_header, struct singly_linked_list *list)
+{
+	if (list->head == EMPTY_OBJ)
+		return;
+	list->head = next(alloc_header, list->head);
+}
+
+void remove_elem(struct allocator_header *alloc_header, struct singly_linked_list *list, uint64_t it)
+{
+	if (list->head == it) {
+		remove_head(alloc_header, list);
+	} else {
+		uint64_t current_element = list->head;
+		uint64_t prev = 0;
+		while ((current_element = next(alloc_header, current_element)) != it) {
+			prev = current_element;
+		}
+		set_next(alloc_header, prev, next(alloc_header, it));
+	}
+}
+
+void push_front(struct allocator_header *alloc_header, struct singly_linked_list *list, uint64_t offset)
+{
+	if (list->head == list->tail) {
+		list->tail = offset;
+	}
+	set_next(alloc_header, offset, list->head);
+	list->head = offset;
+}
+
+void push_back(struct allocator_header *alloc_header, struct singly_linked_list *list, uint64_t offset)
+{
+	set_next(alloc_header, list->tail, offset);
+	list->tail = offset;
+}
+
+void region_allocator_new(struct allocator_header **alloc_header, uint64_t *ptr, size_t size)
+{
+	const uint64_t header_id = 26985;
+	*alloc_header = (struct allocator_header *)ptr;
+	if ((*alloc_header)->magic == header_id)
+		return;
+
+	(*alloc_header)->free_list.head = 0;
+	(*alloc_header)->free_list.tail = 0;
+	(*alloc_header)->alloc_list.head = EMPTY_OBJ;
+	(*alloc_header)->alloc_list.tail = EMPTY_OBJ;
+	(*alloc_header)->magic = header_id;
+
+	struct span_empty se = {.span_base = span_base_create(size - sizeof(struct span_region), SPAN_EMPTY),
+				.next = EMPTY_OBJ};
+	memcpy((*alloc_header)->data, &se, sizeof(se));
+}
+
+uint64_t split(struct allocator_header *alloc_header, uint64_t it, size_t size)
+{
+	if (it == EMPTY_OBJ) {
+		return EMPTY_OBJ;
+	}
+
+	uint64_t free_size = span_get_total_size((struct span_base *)pointer_to_offset(alloc_header->data, it));
+	if (size > free_size + sizeof(struct span_empty)) {
+		return EMPTY_OBJ;
+	}
+	struct span_empty se[2] = {0};
+	size_t target_size[2] = {size, free_size - size};
+	uint64_t target_offset[2] = {it, it + size};
+	se[0].span_base.size_and_type = target_size[0];
+	se[1].span_base.size_and_type = target_size[1];
+	se[0].next = target_offset[1];
+	se[1].next = next(alloc_header, it);
+	memcpy(pointer_to_offset(alloc_header->data, target_offset[0]), &se[0], sizeof(struct span_empty));
+	memcpy(pointer_to_offset(alloc_header->data, target_offset[1]), &se[1], sizeof(struct span_empty));
+
+	/* Tail update */
+	struct span_base *it_sb = (struct span_base *)pointer_to_offset(alloc_header->data, it);
+	uint64_t *tail = NULL;
+	switch (span_get_type(it_sb)) {
+		case SPAN_EMPTY:
+			tail = &(alloc_header->free_list.tail);
+			break;
+		case SPAN_REGION:
+			tail = &(alloc_header->alloc_list.tail);
+			break;
+		default:
+			break;
+	}
+	if (*tail == it) {
+		*tail = target_offset[1];
+	}
+	return target_offset[1];
+}
+
+uint64_t region_allocator_allocate(struct allocator_header *alloc_header, uint64_t size)
+{
+	return EMPTY_OBJ;
+}
+
+void region_allocator_release(struct allocator_header *alloc_header, uint64_t offset)
+{
+	struct span_base *span_b = (struct span_base *)pointer_to_offset(alloc_header->data, offset);
+	// enum span_type span_t = span_get_type(span_b);
+	// assert (span_t == SPAN_EMPTY || span_t == SPAN_REGION);
+	push_back(alloc_header, &alloc_header->free_list, offset);
+	span_b->size_and_type &= SPAN_EXTRA_MASK;
+	remove_elem(alloc_header, &alloc_header->alloc_list, offset);
+}
--- tests/unittest/region_allocator.cpp
+++ tests/unittest/region_allocator.cpp
@@ -3,6 +3,7 @@
 
 /* region_allocator.cpp -- tests region allocator */
 
+#include "region_allocator.h"
 #include "../examples/examples_helpers.h"
 #include "libpmemstream.h"
 #include "libpmemstream_internal.h"
@@ -10,76 +11,8 @@
 #include "unittest.hpp"
 
 #include <iostream>
-#include <stdint.h>
-#include <string.h>
 
 #include <rapidcheck.h>
-
-const uint64_t EMPTY_OBJ = UINT64_MAX;
-
-struct allocator_header {
-	uint64_t magic;
-	uint64_t first_free;	  // first empty span = 0
-	uint64_t first_allocated; // first allocated region offset
-	uint64_t padding[5];
-	uint64_t data[];
-} * header;
-
-/* API */
-uint64_t allocate_region(uint64_t region_size);
-void free_region(uint64_t region_offset);
-uint64_t get_region_iterator();
-uint64_t get_empty_iterator();
-uint64_t next(uint64_t it);
-
-void initialize_memory(uint64_t *ptr, size_t size)
-{
-	const uint64_t header_id = 26985;
-	header = (struct allocator_header *)ptr;
-	if (header->magic == header_id)
-		return;
-
-	header->first_free = 0;
-	header->first_allocated = EMPTY_OBJ;
-	header->magic = header_id;
-
-	span_empty se;
-	se.span_base = span_base_create(size - sizeof(struct span_region), SPAN_EMPTY);
-	memcpy(header->data, &se, sizeof(se));
-}
-
-uint64_t *pointer_to_offset(uint64_t *base, uint64_t offset)
-{
-	return (uint64_t *)((uint8_t *)base + offset);
-}
-
-int is_free_empty(uint64_t *ptr)
-{
-	if (span_get_type((span_base *)&ptr[0]) == SPAN_EMPTY) { // retrieve first element from list
-		return 1;
-	}
-	return 0;
-}
-
-uint64_t split(uint64_t *ptr, size_t size)
-{
-	if (header->first_free == EMPTY_OBJ) {
-		return EMPTY_OBJ;
-	}
-	uint64_t free_size = span_get_total_size((span_base *)pointer_to_offset(header->data, header->first_free));
-	if (size > free_size + sizeof(struct span_empty)) {
-		return EMPTY_OBJ;
-	}
-	span_empty se[2] = {0};
-	size_t target_size[2] = {size, free_size - size}; // size plus metadata size (for empty or region?)
-	uint64_t target_offset[2] = {header->first_free, header->first_free + size};
-	se[0].span_base = span_base_create(target_size[0], SPAN_EMPTY);
-	se[1].span_base = span_base_create(target_size[1], SPAN_EMPTY);
-	se[0].next = target_offset[1];
-	memcpy(pointer_to_offset(header->data, target_offset[0]), &se[0], sizeof(span_empty));
-	memcpy(pointer_to_offset(header->data, target_offset[1]), &se[1], sizeof(span_empty));
-	return 0;
-}
 
 int main(int argc, char *argv[])
 {
@@ -88,7 +21,7 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	u_int64_t size = 16 * 1024;
+	uint64_t size = 16 * 1024;
 	struct pmem2_map *map = example_map_open(argv[1], size);
 	if (map == NULL) {
 		pmem2_perror("pmem2_map");
@@ -100,21 +33,30 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "pmemstream_from_map failed\n");
 		return -1;
 	}
+	struct allocator_header *alloc_header = nullptr;
+
 	uint64_t *spans = stream->data.spans;
-	initialize_memory(spans, size);
+	region_allocator_new(&alloc_header, spans, size);
 
 	return_check ret;
-	ret += rc::check("is free empty", [spans]() {
-		if (is_free_empty(spans) == true) {
-			return true;
-		} else {
-			return false;
-		}
+	ret += rc::check("Initialize new allocator", [alloc_header]() {
+		RC_ASSERT(get_iterator_for_type(alloc_header, SPAN_REGION) == EMPTY_OBJ);
+		RC_ASSERT(get_iterator_for_type(alloc_header, SPAN_EMPTY) != EMPTY_OBJ);
 	});
 
-	split(spans, 2048);
-	/* iterate through free list */
-	split(spans, 512);
+	auto res = split(alloc_header, get_iterator_for_type(alloc_header, SPAN_EMPTY), 512);
+	ret += rc::check("Split empty span", [res]() { RC_ASSERT(res != EMPTY_OBJ); });
+
+	ret += rc::check("is allocated-list empty",
+			 [alloc_header]() { return (get_iterator_for_type(alloc_header, SPAN_REGION) == EMPTY_OBJ); });
+	ret += rc::check("is free-list not empty",
+			 [alloc_header]() { return (get_iterator_for_type(alloc_header, SPAN_EMPTY) != EMPTY_OBJ); });
+	/* iterate through free list and count items */
+	auto it = get_iterator_for_type(alloc_header, SPAN_EMPTY);
+	std::cout << "First element: " << it << std::endl;
+	std::cout << "Next element: " << next(alloc_header, it) << std::endl;
+	std::cout << "Next element: " << next(alloc_header, next(alloc_header, it)) << std::endl;
+	// split(spans, 512);
 	/* iterate through free list */
 
 	pmemstream_delete(&stream);
