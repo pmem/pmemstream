1616ce2d44383802d8315939d171b43684e8161c
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2022-03-15 01:15:32 -0400

    rc test

--- src/singly_linked_list.h
+++ src/singly_linked_list.h
@@ -23,7 +23,7 @@ struct singly_linked_list {
 		(list)->tail = UINT64_MAX;                                                                             \
 	} while (/*CONSTCOND*/ 0)
 
-#define SLIST_INSERT_HEAD(runtime, list, offset, type, field)                                                          \
+#define SLIST_INSERT_HEAD(type, runtime, list, offset, field)                                                          \
 	do {                                                                                                           \
 		if ((list)->tail == UINT64_MAX) {                                                                      \
 			(list)->tail = offset;                                                                         \
@@ -32,12 +32,47 @@ struct singly_linked_list {
 		(list)->head = offset;                                                                                 \
 	} while (0)
 
-#define SLIST_FOREACH(var, runtime, list, type, field)                                                                 \
-	for ((var) = (list)->head; (var) != UINT64_MAX;                                                                \
-	     (var) = ((type *)(pmemstream_offset_to_ptr(runtime, var)))->field)
+#define SLIST_INSERT_TAIL(type, runtime, list, offset, field)                                                          \
+	do {                                                                                                           \
+		((type *)(pmemstream_offset_to_ptr(runtime, offset)))->field = UINT64_MAX;                             \
+		if ((list)->head == UINT64_MAX) {                                                                      \
+			(list)->head = (offset);                                                                       \
+		}                                                                                                      \
+		if ((list)->tail != UINT64_MAX) {                                                                      \
+			((type *)(pmemstream_offset_to_ptr(runtime, (list)->tail)))->field = offset;                   \
+		}                                                                                                      \
+		(list)->tail = (offset);                                                                               \
+	} while (0)
+
+#define SLIST_FOREACH(type, runtime, list, it, field)                                                                  \
+	for ((it) = (list)->head; (it) != UINT64_MAX; (it) = ((type *)(pmemstream_offset_to_ptr(runtime, it)))->field)
+
+#define SLIST_NEXT(type, runtime, it, field) ((type *)(pmemstream_offset_to_ptr(runtime, it)))->field
+
+#define SLIST_REMOVE_HEAD(type, runtime, list, field)                                                                  \
+	do {                                                                                                           \
+		if ((list)->head == UINT64_MAX)                                                                        \
+			break;                                                                                         \
+		if ((list)->tail == (list)->head)                                                                      \
+			(list)->tail = ((type *)(pmemstream_offset_to_ptr((runtime), (list)->head)))->field;           \
+		(list)->head = ((type *)(pmemstream_offset_to_ptr((runtime), (list)->head)))->field;                   \
+	} while (0)
+
+#define SLIST_REMOVE(type, runtime, list, offset, field)                                                               \
+	do {                                                                                                           \
+		if ((list)->head == (offset)) {                                                                        \
+			SLIST_REMOVE_HEAD(type, runtime, list, field);                                                 \
+		} else {                                                                                               \
+			uint64_t curelm = (list)->head;                                                                \
+			while (SLIST_NEXT(type, runtime, curelm, field) != (offset))                                   \
+				curelm = SLIST_NEXT(type, runtime, curelm, field);                                     \
+			if ((SLIST_NEXT(type, runtime, curelm, field) = SLIST_NEXT(type, runtime, offset, field)) ==   \
+			    UINT64_MAX)                                                                                \
+				(list)->tail = UINT64_MAX;                                                             \
+		}                                                                                                      \
+	} while (0)
 
 #ifdef __cplusplus
-
 } /* end extern "C */
 #endif
 
--- tests/unittest/slist.cpp
+++ tests/unittest/slist.cpp
@@ -4,6 +4,8 @@
 #include "singly_linked_list.h"
 #include "unittest.hpp"
 
+#include <rapidcheck.h>
+
 #include <cstring>
 
 struct node {
@@ -11,93 +13,167 @@ struct node {
 	uint64_t next;
 };
 
-int main(int argc, char *argv[])
+std::ostream &operator<<(std::ostream &os, const node &n)
 {
-	uint64_t data[1000];
-	struct pmemstream_data_runtime runtime {
-		.spans = data, .memcpy = NULL, .memset = NULL, .flush = NULL, .drain = NULL, .persist = NULL
-	};
-
-	singly_linked_list list;
-	SLIST_INIT(&list);
-
-	struct node node[3];
-	for (int i = 0; i < 3; i++) {
-		node[i].data = i;
-	}
-
-	memcpy(data, node, sizeof(struct node) * 3);
-
-	SLIST_INSERT_HEAD(&runtime, &list, 0, struct node, next);
-	SLIST_INSERT_HEAD(&runtime, &list, sizeof(struct node), struct node, next);
-	SLIST_INSERT_HEAD(&runtime, &list, 2 * sizeof(struct node), struct node, next);
-
-	uint64_t it = 0;
-	SLIST_FOREACH(it, &runtime, &list, struct node, next)
-	{
-		std::cout << ((struct node *)pmemstream_offset_to_ptr(&runtime, it))->data << std::endl;
-	}
-
-    std::cout << list.head << std::endl;
-    std::cout << list.tail << std::endl;
+	os << n.data << " " << n.next;
+	return os;
 }
 
-// // SPDX-License-Identifier: BSD-3-Clause
-// /* Copyright 2021-2022, Intel Corporation */
+namespace rc
+{
+template <>
+struct Arbitrary<node> {
+	static Gen<node> arbitrary()
+	{
+		return gen::construct<node>(gen::arbitrary<uint64_t>(), gen::arbitrary<uint64_t>());
+	}
+};
+} // namespace rc
 
-// #include "singly_linked_list.h"
-// #include "unittest.hpp"
+void *memcpy_mock(void *dest, const void *src, size_t len, unsigned flags)
+{
+	return NULL;
+}
 
-// #include <rapidcheck.h>
+void *memset_mock(void *dest, int c, size_t len, unsigned flags)
+{
+	return NULL;
+}
 
-// #include <cstring>
+void flush_mock(const void *ptr, size_t size)
+{
+	return;
+}
 
-// struct node {
-// 	uint64_t data;
-// 	uint64_t next;
-// };
+void persist_mock(const void *ptr, size_t size)
+{
+	return;
+}
 
-// std::ostream &operator<<(std::ostream &os, const node &n)
-// {
-// 	os << n.data << " " << n.next;
-// 	return os;
-// }
+void drain_mock(void)
+{
+	return;
+}
 
-// namespace rc
-// {
-// template <>
-// struct Arbitrary<node> {
-// 	static Gen<node> arbitrary()
-// 	{
-// 		return gen::construct<node>(gen::arbitrary<uint64_t>(), gen::just(UINT64_MAX));
-// 	}
-// };
-// } // namespace rc
+int main(int argc, char *argv[])
+{
+	rc::check("XXX", [](const std::vector<struct node> &data) {
+		singly_linked_list list;
+		SLIST_INIT(&list);
 
-// int main(int argc, char *argv[])
-// {
-// 	rc::check("XXX", [](const std::vector<struct node> &data) {
-// 		singly_linked_list list;
-// 		SLIST_INIT(&list);
+		struct pmemstream_data_runtime runtime {
+			.spans = (uint64_t *)data.data(), .memcpy = &memcpy_mock, .memset = &memset_mock,
+			.flush = &flush_mock, .drain = &drain_mock, .persist = &persist_mock
+		};
 
-// 		struct pmemstream_data_runtime runtime {
-// 			.spans = (uint64_t *)data.data(), .memcpy = NULL, .memset = NULL, .flush = NULL, .drain = NULL,
-// 			.persist = NULL
-// 		};
+		uint64_t offset = 0;
+		for (size_t i = 0; i < data.size(); ++i) {
+			SLIST_INSERT_HEAD(struct node, &runtime, &list, offset, next);
+			offset += sizeof(struct node);
+		}
 
-// 		uint64_t offset = 0;
-// 		for (auto &d : data) {
-// 			SLIST_INSERT_HEAD(&runtime, &list, offset, struct node, next);
-// 			offset += sizeof(struct node);
-// 		}
+		uint64_t it = 0;
+		auto rit = data.rbegin();
+		SLIST_FOREACH(struct node, &runtime, &list, it, next)
+		{
+			RC_ASSERT(((node *)(pmemstream_offset_to_ptr(&runtime, it)))->data == rit->data);
+			rit++;
+		}
+	});
 
-// 		uint64_t it = 0;
-// 		uint64_t count = 0;
-// 		SLIST_FOREACH(it, &runtime, &list, struct node, next)
-// 		{
-// 			RC_ASSERT(((struct node *)pmemstream_offset_to_ptr(&runtime, it))->data ==
-// 				  data[data.size() - 1 - count].data);
-// 		}
-// 	});
-// }
+	rc::check("Push back", [](const std::vector<struct node> &data) {
+		singly_linked_list list;
+		SLIST_INIT(&list);
+		struct pmemstream_data_runtime runtime {
+			.spans = (uint64_t *)data.data(), .memcpy = &memcpy_mock, .memset = &memset_mock,
+			.flush = &flush_mock, .drain = &drain_mock, .persist = &persist_mock
+		};
 
+		uint64_t offset = 0;
+		for (size_t i = 0; i < data.size(); ++i) {
+			SLIST_INSERT_TAIL(struct node, &runtime, &list, offset, next);
+			offset += sizeof(struct node);
+		}
+
+		uint64_t it = 0;
+		auto v_it = data.begin();
+		SLIST_FOREACH(struct node, &runtime, &list, it, next)
+		{
+			RC_ASSERT(((node *)(pmemstream_offset_to_ptr(&runtime, it)))->data == v_it->data);
+			v_it++;
+		}
+	});
+
+	rc::check("Remove head", [](const std::vector<struct node> &data) {
+		singly_linked_list list;
+		SLIST_INIT(&list);
+		struct pmemstream_data_runtime runtime {
+			.spans = (uint64_t *)data.data(), .memcpy = &memcpy_mock, .memset = &memset_mock,
+			.flush = &flush_mock, .drain = &drain_mock, .persist = &persist_mock
+		};
+
+		uint64_t offset = 0;
+		for (size_t i = 0; i < data.size(); ++i) {
+			SLIST_INSERT_TAIL(struct node, &runtime, &list, offset, next);
+			offset += sizeof(struct node);
+		}
+
+		std::vector<struct node> mod_data = data;
+		std::vector<struct node>::iterator it = mod_data.begin();
+		if (it != mod_data.end())
+			it = mod_data.erase(it);
+		if (list.head != UINT64_MAX) {
+			SLIST_REMOVE_HEAD(struct node, &runtime, &list, next);
+		}
+
+		uint64_t l_it = list.head;
+		if (l_it == UINT64_MAX)
+			return;
+
+		auto v_it = mod_data.begin();
+		while (v_it != mod_data.end()) {
+			RC_ASSERT(((node *)(pmemstream_offset_to_ptr(&runtime, l_it)))->data == v_it->data);
+			l_it = ((node *)(pmemstream_offset_to_ptr(&runtime, l_it)))->next;
+			v_it++;
+		}
+	});
+
+	rc::check("Random remove", [](const std::vector<struct node> &data) {
+		singly_linked_list list;
+		SLIST_INIT(&list);
+		struct pmemstream_data_runtime runtime {
+			.spans = (uint64_t *)data.data(), .memcpy = &memcpy_mock, .memset = &memset_mock,
+			.flush = &flush_mock, .drain = &drain_mock, .persist = &persist_mock
+		};
+
+		uint64_t offset = 0;
+		for (size_t i = 0; i < data.size(); ++i) {
+			SLIST_INSERT_TAIL(struct node, &runtime, &list, offset, next);
+			offset += sizeof(struct node);
+		}
+
+		std::vector<struct node> mod_data(data);
+		auto random_item_pos = mod_data.size() ? static_cast<size_t>(rand()) % mod_data.size() : 0;
+		std::vector<struct node>::iterator r_it = mod_data.begin();
+		auto l_it = list.head;
+		size_t i;
+		for (i = 0; i < random_item_pos; ++i) {
+			r_it++;
+			l_it = ((node *)(pmemstream_offset_to_ptr(&runtime, l_it)))->next;
+			RC_ASSERT(((node *)(pmemstream_offset_to_ptr(&runtime, l_it)))->data == r_it->data);
+		}
+
+		if (i != 0) {
+			mod_data.erase(r_it);
+			SLIST_REMOVE(struct node, &runtime, &list, l_it, next);
+
+			l_it = list.head;
+			auto v_it = mod_data.begin();
+			SLIST_FOREACH(struct node, &runtime, &list, l_it, next)
+			{
+				RC_ASSERT(((node *)(pmemstream_offset_to_ptr(&runtime, l_it)))->data == v_it->data);
+				v_it++;
+			}
+		}
+	});
+}
