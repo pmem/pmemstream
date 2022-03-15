99009ea246da9f1de8ab2ab79ecf180b66e73314
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2022-03-15 01:03:40 -0400

    macro based impl

--- src/singly_linked_list.h
+++ src/singly_linked_list.h
@@ -8,20 +8,34 @@
 
 #include "libpmemstream_internal.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct singly_linked_list {
 	uint64_t head;
 	uint64_t tail;
 };
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#define SLIST_INIT(list)                                                                                               \
+	do {                                                                                                           \
+		(list)->head = UINT64_MAX;                                                                             \
+		(list)->tail = UINT64_MAX;                                                                             \
+	} while (/*CONSTCOND*/ 0)
 
-// pmemstream_data_runtime
-void push_front(struct pmemstream_data_runtime *data, struct singly_linked_list *list, uint64_t offset);
-void push_back(struct pmemstream_data_runtime *data, struct singly_linked_list *list, uint64_t offset);
-void remove_head(struct pmemstream_data_runtime *data, struct singly_linked_list *list);
-uint64_t next(struct pmemstream_data_runtime *data, uint64_t it);
+#define SLIST_INSERT_HEAD(runtime, list, offset, type, field)                                                          \
+	do {                                                                                                           \
+		if ((list)->tail == UINT64_MAX) {                                                                      \
+			(list)->tail = offset;                                                                         \
+		}                                                                                                      \
+		((type *)(pmemstream_offset_to_ptr(runtime, offset)))->field = (list)->head;                           \
+		(list)->head = offset;                                                                                 \
+	} while (0)
+
+#define SLIST_FOREACH(var, runtime, list, type, field)                                                                 \
+	for ((var) = (list)->head; (var) != UINT64_MAX;                                                                \
+	     (var) = ((type *)(pmemstream_offset_to_ptr(runtime, var)))->field)
+
 #ifdef __cplusplus
 
 } /* end extern "C */
