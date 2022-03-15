00da7b41537d44c12b9786e75440998e888449f7
Igor Chorazewicz <Igor.Chorazewicz@intel.com>
2022-03-15 01:15:32 -0400

    rc test

--- src/singly_linked_list.h
+++ src/singly_linked_list.h
@@ -32,9 +32,65 @@ struct singly_linked_list {
 		(list)->head = offset;                                                                                 \
 	} while (0)
 
-#define SLIST_FOREACH(var, runtime, list, type, field)                                                                 \
-	for ((var) = (list)->head; (var) != UINT64_MAX;                                                                \
-	     (var) = ((type *)(pmemstream_offset_to_ptr(runtime, var)))->field)
+#define SLIST_INSERT_TAIL(runtime, list, offset, type, field)                                                          \
+	do {                                                                                                           \
+		((type *)(pmemstream_offset_to_ptr(runtime, offset)))->field = UINT64_MAX;                             \
+		if ((list)->head == UINT64_MAX) {                                                                      \
+			(list)->head = (offset);                                                                       \
+		}                                                                                                      \
+		if ((list)->tail != UINT64_MAX) {                                                                      \
+			((type *)(pmemstream_offset_to_ptr(runtime, (list)->tail)))->field = offset;                   \
+		}                                                                                                      \
+		(list)->tail = (offset);                                                                               \
+	} while (0)
+
+#define SLIST_FOREACH(type, runtime, list, it, field)                                                                 \
+	for ((it) = (list)->head; (it) != UINT64_MAX;                                                                \
+	     (it) = ((type *)(pmemstream_offset_to_ptr(runtime, it)))->field)
+
+#define	SLIST_NEXT(runtime, it, field) (it = (type *)(pmemstream_offset_to_ptr(runtime, it)))->field)
+
+#define SLIST_REMOVE_HEAD(runtime, list, type, field)                                                                  \
+	do {                                                                                                           \
+		if ((list)->head == UINT64_MAX)                                                                        \
+			break;                                                                                         \
+		(list)->head = ((type *)(pmemstream_offset_to_ptr((runtime), (list)->head)))->field;                   \
+	} while (0)
+
+// ((node *)(pmemstream_offset_to_ptr(&runtime, it)))->data
+#define SLIST_REMOVE(runtime, list, offset, type, field)                                                               \
+	do {                                                                                                           \
+		if ((list)->head == (offset)) {                                                                        \
+			SLIST_REMOVE_HEAD(runtime, list, type, field);                                                 \
+		} else {                                                                                               \
+			uint64_t current_element = (list)->head;                                                       \
+			uint64_t prev = 0;                                                                             \
+			(void)prev;                                                                                    \
+			while ((current_element =                                                                      \
+					((type *)(pmemstream_offset_to_ptr(runtime, current_element)))->field) !=      \
+			       offset) {                                                                               \
+				prev = current_element;                                                                \
+			}                                                                                              \
+			((type *)(pmemstream_offset_to_ptr(runtime, prev)))->field =                                   \
+				((type *)(pmemstream_offset_to_ptr(runtime, current_element)))->field;                 \
+		}                                                                                                      \
+	} while (0)
+// void remove_elem(struct allocator_header *alloc_header, struct singly_linked_list *list, uint64_t it)
+
+/*
+if ((list)->head == (offset)) {                                                                        \
+			SLIST_REMOVE_HEAD(runtime, list, type, field); \
+		} else { \
+			uint64_t curelm = (list)->head; \
+			uint64_t prev = 0; \
+			while ((current_element = \
+					((type *)(pmemstream_offset_to_ptr(runtime, (list)->head)))->field) !=
+it) {   \
+				prev = current_element; \
+				if ((curelm->field.sqe_next = curelm->field.sqe_next->field.sqe_next) == NULL) \
+					(head)->sqh_last = &(curelm)->field.sqe_next; \
+			} \
+*/
 
 #ifdef __cplusplus
 
