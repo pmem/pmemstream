744f98a0e4424c0da0d080de35995e3020212715
Karolina Bober <karolina.bober@intel.com>
2022-03-16 18:59:27 +0100

    Check if new list points to invalid elements (UINT64_MAX)

---
+++ tests/unittest/slist_simple_test.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021-2022, Intel Corporation */
+
+#include "singly_linked_list.h"
+#include "unittest.hpp"
+
+int main(int argc, char *argv[])
+{
+	singly_linked_list list;
+	SLIST_INIT(&list);
+
+	UT_ASSERTeq((list)->head, UINT64_MAX);
+	UT_ASSERTeq((list)->tail, UINT64_MAX);
+}
