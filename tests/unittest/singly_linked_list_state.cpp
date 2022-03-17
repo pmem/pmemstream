a3c87e1a91af8c23780daf38b5ba683a08bc0d8d
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-03-17 16:11:00 +0100

    tests: add rc stateful tests for singly linked list

---
+++ tests/unittest/singly_linked_list_state.cpp
@@ -0,0 +1,247 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+#include "singly_linked_list.h"
+
+#include <rapidcheck.h>
+
+#include "rapidcheck_helpers.hpp"
+#include "stream_helpers.hpp"
+#include "unittest.hpp"
+
+/* in some cases we expect list to be ordered */
+enum ordering { unordered, ascending, descending };
+
+/* to avoid null'ed data base we use arbitrary value
+ * (UBSAN checker doesn't like "applying non-zero offset to null pointer") */
+#define TEST_SLIST_DATA_BASE (1024UL)
+
+struct node {
+	node(uint64_t d) : data(d), next(SLIST_INVALID_OFFSET)
+	{
+	}
+	uint64_t data;
+	uint64_t next;
+};
+
+/*
+ * Simplified model of the list of nodes, where elements are represented as incremented
+ * counter's value (which is unique). Each created node has this counter's value set as data.
+ */
+struct singly_linked_list_model {
+	singly_linked_list_model(ordering o) : list(), counter(0), order(o)
+	{
+	}
+	std::list<uint64_t> list;
+	size_t counter;
+	ordering order;
+};
+
+/*
+ * SUT struct with NULL-ed begining of the spans.
+ * It simplifies evaluating the pointer of each node (for the sake of testing).
+ */
+struct singly_linked_list_test {
+	singly_linked_list_test()
+	{
+		SLIST_INIT(&sut);
+		runtime = {.base = (void *)TEST_SLIST_DATA_BASE,
+			   .memcpy = &memcpy_mock,
+			   .memset = &memset_mock,
+			   .flush = &flush_mock,
+			   .drain = &drain_mock,
+			   .persist = &persist_mock};
+	}
+
+	struct singly_linked_list sut;
+	struct pmemstream_runtime runtime;
+};
+
+using singly_linked_list_command = rc::state::Command<singly_linked_list_model, singly_linked_list_test>;
+
+struct singly_linked_list_insert_command : singly_linked_list_command {
+	struct node *n = NULL;
+
+	singly_linked_list_insert_command(const singly_linked_list_model &m)
+	{
+		n = new struct node(m.counter);
+	}
+
+	~singly_linked_list_insert_command()
+	{
+		/* RC commands live for the whole TC lifetime (unless they don't fulfill preconditions),
+		 * so it should be safe to cleanup in command's dtor. */
+		delete n;
+	}
+};
+
+struct rc_insert_head_command : singly_linked_list_insert_command {
+	explicit rc_insert_head_command(const singly_linked_list_model &m) : singly_linked_list_insert_command(m)
+	{
+	}
+
+	void apply(singly_linked_list_model &m) const override
+	{
+		m.list.push_front(n->data);
+		/* 'apply' function is executed after the 'run', so the model is updated
+		 * after the successful insert in SUT's (and model's) list. */
+		m.counter++;
+	}
+
+	void run(const singly_linked_list_model &m, singly_linked_list_test &s) const override
+	{
+		SLIST_INSERT_HEAD(struct node, &s.runtime, &s.sut, (uint64_t)n - TEST_SLIST_DATA_BASE, next);
+	}
+};
+
+struct rc_insert_tail_command : singly_linked_list_insert_command {
+	explicit rc_insert_tail_command(const singly_linked_list_model &m) : singly_linked_list_insert_command(m)
+	{
+	}
+
+	void apply(singly_linked_list_model &m) const override
+	{
+		m.list.push_back(n->data);
+		/* 'apply' function is executed after the 'run', so the model is updated
+		 * after the successful insert in SUT's (and model's) list. */
+		m.counter++;
+	}
+
+	void run(const singly_linked_list_model &m, singly_linked_list_test &s) const override
+	{
+		SLIST_INSERT_TAIL(struct node, &s.runtime, &s.sut, (uint64_t)n - TEST_SLIST_DATA_BASE, next);
+	}
+};
+
+struct rc_remove_head_command : singly_linked_list_command {
+	void checkPreconditions(const singly_linked_list_model &m) const override
+	{
+		RC_PRE(m.list.size() > 0);
+	}
+
+	void apply(singly_linked_list_model &m) const override
+	{
+		m.list.pop_front();
+	}
+
+	void run(const singly_linked_list_model &m, singly_linked_list_test &s) const override
+	{
+		SLIST_REMOVE_HEAD(struct node, &s.runtime, &s.sut, next);
+	}
+};
+
+struct rc_remove_command : singly_linked_list_command {
+	uint64_t data_to_remove;
+
+	explicit rc_remove_command(const singly_linked_list_model &m)
+	{
+		data_to_remove = *rc::gen::elementOf(m.list);
+	}
+
+	void checkPreconditions(const singly_linked_list_model &m) const override
+	{
+		/* this check is run not only after ctor but also in case of shrinking */
+
+		RC_PRE(m.list.size() > 0);
+		/* make sure it's still there before we apply it */
+		auto found = std::find(m.list.begin(), m.list.end(), data_to_remove);
+		RC_PRE(found != m.list.end());
+	}
+
+	void apply(singly_linked_list_model &m) const override
+	{
+		m.list.remove(data_to_remove);
+	}
+
+	void run(const singly_linked_list_model &m, singly_linked_list_test &s) const override
+	{
+		/* find offset of a node with .data == 'data_to_remove' */
+		auto it = s.sut.head;
+		SLIST_FOREACH(struct node, &s.runtime, &s.sut, it, next)
+		{
+			auto it_data = (SLIST_GET_PTR(struct node, &s.runtime, it))->data;
+			if (it_data == data_to_remove) {
+				break;
+			}
+		}
+		RC_ASSERT(it != SLIST_INVALID_OFFSET);
+
+		SLIST_REMOVE(struct node, &s.runtime, &s.sut, it, next);
+	}
+};
+
+struct rc_foreach_command : singly_linked_list_command {
+	void run(const singly_linked_list_model &m, singly_linked_list_test &s) const override
+	{
+		auto m_it = m.list.begin();
+		auto sut_it = s.sut.head;
+		auto sut_it_next = SLIST_INVALID_OFFSET;
+		SLIST_FOREACH(struct node, &s.runtime, &s.sut, sut_it, next)
+		{
+			auto sut_it_data = (SLIST_GET_PTR(struct node, &s.runtime, sut_it))->data;
+			sut_it_next = SLIST_NEXT(struct node, &s.runtime, sut_it, next);
+
+			RC_ASSERT(sut_it_data == *m_it);
+			if (sut_it_next != SLIST_INVALID_OFFSET) {
+				/* we are not at the end of the list yet */
+
+				auto next_data = (SLIST_GET_PTR(struct node, &s.runtime, sut_it_next))->data;
+				if (m.order == ordering::ascending) {
+					RC_ASSERT(sut_it_data < next_data);
+				} else if (m.order == ordering::descending) {
+					RC_ASSERT(sut_it_data > next_data);
+				}
+			}
+
+			m_it++;
+		}
+		RC_ASSERT(sut_it_next == SLIST_INVALID_OFFSET);
+	}
+};
+
+std::ostream &operator<<(std::ostream &os, const node &n)
+{
+	os << "data: " << n.data << ", next: " << n.next;
+	return os;
+}
+
+int main(int argc, char *argv[])
+{
+	return run_test([&] {
+		// XXX: can we easily change this only e.g. for memcheck...?
+		// std::string rapidcheck_config = "noshrink=1 max_success=200"; //  max_size=20
+		// env_setter setter("RC_PARAMS", rapidcheck_config, false);
+
+		return_check ret;
+		ret += rc::check("Random inserts and removes should not break a list", []() {
+			singly_linked_list_model model(ordering::unordered);
+			singly_linked_list_test sut;
+
+			rc::state::check(
+				model, sut,
+				rc::state::gen::execOneOfWithArgs<rc_insert_tail_command, rc_insert_head_command,
+								  rc_remove_head_command, rc_remove_command,
+								  rc_foreach_command>());
+		});
+
+		ret += rc::check("Tail inserts and random removes should produce ascending list", []() {
+			singly_linked_list_model model(ordering::ascending);
+			singly_linked_list_test sut;
+
+			rc::state::check(
+				model, sut,
+				rc::state::gen::execOneOfWithArgs<rc_insert_tail_command, rc_remove_head_command,
+								  rc_remove_command, rc_foreach_command>());
+		});
+
+		ret += rc::check("Head inserts and random removes should produce descending list", []() {
+			singly_linked_list_model model(ordering::descending);
+			singly_linked_list_test sut;
+
+			rc::state::check(
+				model, sut,
+				rc::state::gen::execOneOfWithArgs<rc_insert_head_command, rc_remove_head_command,
+								  rc_remove_command, rc_foreach_command>());
+		});
+	});
+}
