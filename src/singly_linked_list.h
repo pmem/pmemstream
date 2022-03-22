32972bda7191b5826026aaa1588f06ca1b18bacd
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-03-22 19:50:29 +0100

    fix SLIST_REMOVE macro
    after removal of the last element the new tail should be set to
    the current element, not to the removed offset.

--- src/singly_linked_list.h
+++ src/singly_linked_list.h
@@ -75,11 +75,11 @@ struct singly_linked_list {
 			SLIST_REMOVE_HEAD(type, runtime, list, field);                                                 \
 		} else {                                                                                               \
 			uint64_t curelm = (list)->head;                                                                \
-			while (SLIST_NEXT(type, runtime, curelm, field) != (offset))                                   \
+			while (SLIST_NEXT(type, runtime, curelm, field) != offset)                                     \
 				curelm = SLIST_NEXT(type, runtime, curelm, field);                                     \
 			if ((SLIST_NEXT(type, runtime, curelm, field) = SLIST_NEXT(type, runtime, offset, field)) ==   \
 			    SLIST_INVALID_OFFSET)                                                                      \
-				(list)->tail = offset;                                                                 \
+				(list)->tail = curelm;                                                                 \
 		}                                                                                                      \
 	} while (0)
 
