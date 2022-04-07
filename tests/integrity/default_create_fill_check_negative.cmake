8be22d8b843c8e70279a3ce6d75fd29fc03c6bc6
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-04-07 13:51:40 +0200

    tests: add pmreorder test for allocating/freeing multiple regions
    it checks consistency of pmemstream when region_allocate/_free
    was e.g. incomplete.

--- tests/integrity/default_create_fill_check_negative.cmake
+++ tests/integrity/default_create_fill_check_negative.cmake
@@ -8,10 +8,10 @@ setup()
 
 execute(${EXECUTABLE} create ${DIR}/pretest_testfile)
 execute(${EXECUTABLE} fill ${DIR}/pretest_testfile)
-execute(${EXECUTABLE} check ${DIR}/pretest_testfile)
+execute(${EXECUTABLE} check_no_recovery ${DIR}/pretest_testfile)
 
 execute(${EXECUTABLE} create ${DIR}/testfile)
 pmreorder_create_store_log(${DIR}/testfile ${EXECUTABLE} fill ${DIR}/testfile)
-pmreorder_execute(${EXPECT_FAILURE} ReorderAccumulative ${SRC_DIR}/pmreorder.conf ${EXECUTABLE} check_without_recovery)
+pmreorder_execute(${EXPECT_FAILURE} ReorderAccumulative ${SRC_DIR}/integrity/pmreorder.conf ${EXECUTABLE} check_no_recovery)
 
 finish()
---
+++ tests/integrity/multi_region_pmreorder.cpp
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2022, Intel Corporation */
+
+#include "libpmemstream_internal.h"
+#include "random_helpers.hpp"
+#include "stream_helpers.hpp"
+#include "unittest.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+#define TEST_COMMANDS_COUNT (50UL)
+
+void fill(test_config_type test_config, size_t cmd_count = TEST_COMMANDS_COUNT)
+{
+	bool no_truncate = false;
+	auto stream =
+		make_pmemstream(test_config.filename, test_config.block_size, test_config.stream_size, no_truncate);
+
+	const size_t region_size = TEST_DEFAULT_BLOCK_SIZE;
+	std::vector<struct pmemstream_region> regions;
+
+	for (size_t i = 0; i < cmd_count; i++) {
+		auto command = rnd_generator() % 2; /* allocate & free */
+
+		if (command == 0) {
+			struct pmemstream_region region;
+			int ret = pmemstream_region_allocate(stream.get(), region_size, &region);
+			UT_ASSERTeq(ret, 0);
+			regions.push_back(region);
+		} else if (command == 1) {
+			/* free (only if preceeded by any, non-freed allocation) */
+			// XXX: get rid of the "true" statement; _free is not working properly
+			if (true || regions.size() == 0) {
+				continue;
+			}
+
+			size_t region_pos = rnd_generator() % regions.size();
+			auto region = regions[region_pos];
+			int ret = pmemstream_region_free(stream.get(), region);
+			UT_ASSERTeq(ret, 0);
+			regions.erase(regions.begin() + static_cast<long>(region_pos));
+		}
+	}
+}
+
+void check_consistency(test_config_type test_config)
+{
+	auto copy_path = copy_file(test_config.filename);
+	bool no_truncate = false;
+	bool no_init = false;
+
+	struct pmemstream_test_base s(copy_path, test_config.block_size, test_config.stream_size, no_truncate, no_init);
+
+	auto riter = s.sut.region_iterator();
+	int region_counter = -1;
+	int ret = 0;
+	do {
+		++region_counter;
+		struct pmemstream_region region;
+		ret = pmemstream_region_iterator_next(riter.get(), &region);
+	} while (ret == 0);
+
+	// XXX: investigate consistency assurance
+	// struct pmemstream_region region;
+	// ret = pmemstream_region_allocate(s.sut.c_ptr(), TEST_DEFAULT_BLOCK_SIZE, &region);
+	// UT_ASSERTeq(ret, 0);
+	// UT_ASSERTeq(static_cast<size_t>(region_counter + 1), s.helpers.count_regions());
+
+	// XXX: uncomment when region_free will work properly
+	// ret = pmemstream_region_free(s.sut.c_ptr(), region);
+	// UT_ASSERTeq(ret, 0);
+	// UT_ASSERTeq(static_cast<size_t>(region_counter), s.helpers.count_regions());
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		std::cout << "Usage: " << argv[0] << "<create|fill|check> file-path" << std::endl;
+		return -1;
+	}
+
+	struct test_config_type test_config;
+	std::string mode = argv[1];
+	test_config.filename = argv[2];
+	/* requested region_size in this test is of "block_size", but it's actually double that, because of aligning */
+	test_config.stream_size = STREAM_METADATA_SIZE + 2 * TEST_DEFAULT_BLOCK_SIZE * (TEST_COMMANDS_COUNT + 1);
+
+	return run_test(test_config, [&] {
+		if (mode == "create") {
+			bool truncate = true;
+			bool do_init = true;
+			struct pmemstream_test_base stream(test_config.filename, test_config.block_size,
+							   test_config.stream_size, truncate, do_init);
+
+		} else if (mode == "fill") {
+			init_random();
+			fill(test_config);
+
+		} else if (mode == "check") {
+			check_consistency(test_config);
+
+		} else {
+			UT_FATAL("Wrong mode given!\nUsage: %s <create|fill|check> file-path", argv[0]);
+		}
+	});
+}
