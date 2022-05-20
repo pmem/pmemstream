7294a96fdf68b429285e8fb80ee9e3e6a5b3efe8
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2022-05-20 14:42:14 +0200

    util: remove popcount utility functions
    along with module's tests.
    
    Since #183 this code is not used in any way.

--- src/common/util.h
+++ src/common/util.h
@@ -18,25 +18,4 @@
 /* XXX: add support for different architectures. */
 #define CACHELINE_SIZE (64ULL)
 
-static inline unsigned char util_popcount64(uint64_t value)
-{
-	return (unsigned char)__builtin_popcountll(value);
-}
-
-static inline size_t util_popcount_memory(const uint8_t *data, size_t size)
-{
-	size_t count = 0;
-	size_t i = 0;
-
-	if (data != NULL) {
-		for (; i < ALIGN_DOWN(size, sizeof(uint64_t)); i += sizeof(uint64_t)) {
-			count += util_popcount64(*(const uint64_t *)(data + i));
-		}
-		for (; i < size; i++) {
-			count += util_popcount64(data[i]);
-		}
-	}
-	return count;
-}
-
 #endif /* LIBPMEMSTREAM_UTIL_H */
