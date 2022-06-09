7a9fce5a8c6e0cbcbbd2ba10061016101b3cd332
Pawel Karczewski <pawel.karczewski@intel.com>
2022-06-09 12:52:41 +0200

    Add RapidCheck generator of stream filled with data

--- tests/unittest/concurrent_iterate.cpp
+++ tests/unittest/concurrent_iterate.cpp
@@ -10,7 +10,10 @@
 #include "thread_helpers.hpp"
 #include "unittest.hpp"
 
-static constexpr size_t max_concurrency = 12;
+static constexpr size_t max_concurrency = 8;
+
+/* Stream big enough to fit test cases */
+static constexpr size_t STREAM_SIZE = TEST_DEFAULT_STREAM_SIZE * rc::DEFAULT_ELEMENT_COUNT * rc::DEFAULT_ELEMENT_COUNT;
 
 int main(int argc, char *argv[])
 {
@@ -21,27 +24,31 @@ int main(int argc, char *argv[])
 
 	struct test_config_type test_config;
 	test_config.filename = std::string(argv[1]);
+	test_config.stream_size = STREAM_SIZE;
 
 	return run_test(test_config, [&] {
 		return_check ret;
 
-		ret += rc::check(
-			"verify if each concurrent iteration observes the same data",
-			[&](const std::vector<std::string> &data, bool reopen,
-			    ranged<size_t, 1, max_concurrency> concurrency) {
-				pmemstream_test_base stream(get_test_config().filename, get_test_config().block_size,
-							    get_test_config().stream_size);
-				auto region = stream.helpers.initialize_single_region(TEST_DEFAULT_REGION_SIZE, data);
+		ret += rc::check("verify if each concurrent iteration observes the same data",
+				 [&](pmemstream_with_multi_non_empty_regions &&stream, bool reopen,
+				     ranged<size_t, 1, max_concurrency> concurrency) {
+					 if (reopen)
+						 stream.reopen();
 
-				if (reopen)
-					stream.reopen();
+					 auto regions = stream.helpers.get_regions();
 
-				std::vector<std::vector<std::string>> threads_data(concurrency);
-				parallel_exec(concurrency, [&](size_t tid) {
-					threads_data[tid] = stream.helpers.get_elements_in_region(region);
-				});
+					 for (const auto &region : regions) {
+						 syncthreads_barrier syncthreads(concurrency);
 
-				UT_ASSERT(all_of(threads_data, predicates::equal(data)));
-			});
+						 std::vector<std::vector<std::string>> threads_data(concurrency);
+						 parallel_exec(concurrency, [&](size_t tid) {
+							 syncthreads();
+							 threads_data[tid] =
+								 stream.helpers.get_elements_in_region(region);
+						 });
+
+						 UT_ASSERT(all_of(threads_data, predicates::equal(threads_data[0])));
+					 }
+				 });
 	});
 }
