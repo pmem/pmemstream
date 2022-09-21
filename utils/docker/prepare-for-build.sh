d0a3b270e555defc120998fbe83db4d523c13e4c
Karolina Bober <karolina.bober@intel.com>
2022-09-21 10:27:14 +0200

    Check if code was compiled with clang and set gcovexe accordingly

--- utils/docker/prepare-for-build.sh
+++ utils/docker/prepare-for-build.sh
@@ -32,17 +32,14 @@ function workspace_cleanup() {
 function upload_codecov() {
 	printf "\n$(tput setaf 1)$(tput setab 7)COVERAGE ${FUNCNAME[0]} START$(tput sgr 0)\n"
 
-	# check if code was compiled with clang
+	# run codecov using proper gcov executable
+	# we rely on parsed report on codecov.io
 	clang_used=$(cmake -LA -N . | grep -e "CMAKE_C.*_COMPILER" | grep clang | wc -c)
 	if [[ ${clang_used} -gt 0 ]]; then
-		# XXX: llvm-cov not supported
-		echo "Warning: Llvm-cov is not supported"
-		return 0
+		/opt/scripts/codecov --flags ${1} -v --nonZero --gcov --gcovExecutable "llvm" ${IGNORE_PATHS} --rootDir . --clean
+	else
+		/opt/scripts/codecov --flags ${1} --nonZero --gcov ${IGNORE_PATHS} --rootDir . --clean
 	fi
-
-	# run codecov using gcov
-	# we rely on parsed report on codecov.io
-	/opt/scripts/codecov --flags ${1} --nonZero --gcov ${IGNORE_PATHS} --rootDir . --clean
 
 	echo "Check for any leftover gcov files"
 	leftover_files=$(find . -name "*.gcov")
